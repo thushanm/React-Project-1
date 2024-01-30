cf4930a9916448e31c559303c21b62bd32e25aad
thushanm <122750947+thushanm@users.noreply.github.com>
2024-01-31 02:03:03 +0530

    1 st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import './output.css';
+import './index.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+        <h1 className="text-lime-400 font-size: 8rem font-normal hover:font-bold">Thushan madhushara</h1>
+        <div className="bg-amber-300 w-100">
+
+<h3> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta facilis fugit minus nam, officia quibusdam repellendus.
+    Distinctio dolores laborum omnis, optio porro quisquam. Ab consequuntur enim officia quasi tempora ut? </h3>
+
+            <div className="w-100 h-100 flex"><img className="flex" src={"https://wallpaperaccess.com/full/191333.jpg"}/>
+
+                </div>
+
+        </div>
+
     </div>
   );
 }
