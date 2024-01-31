a751b10f5f29ee1a1cc552fb278e72833638872b
thushanm <122750947+thushanm@users.noreply.github.com>
2024-01-31 14:39:20 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './output.css';
 import './index.css';
 import './App.css';
+import {ExpenseItem} from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
 
 
-
-        <h1 className="text-lime-400 font-size: 8rem font-normal hover:font-bold">Thushan madhushara</h1>
-        <div className="bg-amber-300 w-100">
-
-<h3> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta facilis fugit minus nam, officia quibusdam repellendus.
-    Distinctio dolores laborum omnis, optio porro quisquam. Ab consequuntur enim officia quasi tempora ut? </h3>
-
-            <div className="w-100 h-100 flex"><img className="flex" src={"https://wallpaperaccess.com/full/191333.jpg"}/>
-
-                </div>
-
-        </div>
+        <ExpenseItem></ExpenseItem>
 
     </div>
   );
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import './Expense.css'
+
+export function ExpenseItem(){
+    return (
+
+        <div className="expense-item">
+
+
+
+            <div>March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div class="expense-item__price">$294.96</div>
+            </div>
+
+
+
+</div>
+    );
+
+}
