5d9d0c4a12bcfe47bce1e82e7ff3ff948487deef
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-01 14:14:33 +0530

    addd propos data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import './output.css';
 import './index.css';
 import './App.css';
+import './components/Expense.css'
 import {ExpenseItem} from "./components/ExpenseItem";
 
 function App() {
+
+
+
+    const data = [{
+        id:'001',
+        title:'mazda',
+        amount:'$456.96',
+        date:'2024.01.31',
+
+
+    },{
+        id:'002',
+        title:'Benz',
+        amount:'$524.36',
+        date:'2024.01.31',
+
+
+    },{
+        id:'003',
+        title:'BMW',
+        amount:'$668.6',
+        date:'2024.01.31',
+
+
+    }]
+
   return (
     <div className="App">
 
 
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem  title={data[0].title} amount={data[0].amount} date={data[0].date}></ExpenseItem>
+        <ExpenseItem  title={data[1].title} amount={data[1].amount} date={data[1].date}></ExpenseItem>
+        <ExpenseItem  title={data[2].title} amount={data[2].amount} date={data[2].date}></ExpenseItem>
+
+
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './Expense.css'
 
-export function ExpenseItem(){
+export function ExpenseItem(props ){
+
+
+
+
+
     return (
 
         <div className="expense-item">
 
 
 
-            <div>March 28th 2021</div>
+            <div>{props.date}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div class="expense-item__price">$294.96</div>
+                <h2>{props.title}</h2>
+                <div class="expense-item__price">{props.amount}</div>
             </div>
 
 
