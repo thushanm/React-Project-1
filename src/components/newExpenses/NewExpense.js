9a0b367e3bb422357c2f4d41fb8ebfee76c12782
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-15 22:57:12 +0530

    add form

---
+++ src/components/newExpenses/NewExpense.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './newExpense.css';
+export  const NewExpense =()=>{
+
+    return <div className="new-expense">
+<form></form>
+
+    </div>
+}
---
+++ src/components/newExpenses/expenseForm.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './expenseForm.css'
+
+
+const ExpenseForm=()=>{
+return <form>
+    <div className="new-expense__controller">
+    <div className="new-expense__controller">
+        
+        <label>Titele</label>
+        </div>
+    </div>
+</form>
+
+}
